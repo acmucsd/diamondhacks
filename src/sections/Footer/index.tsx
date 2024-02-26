6f2596c7bed7b43ed8476da9431ea3461217629f
newracket <newracket@users.noreply.github.com>
2024-02-26 14:58:53 -0800

    Add link to ACM website in Footer module

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
+      <a className={styles.left} href="https://acmurl.com/" target="_blank">
         <Image src="/acm-logo.png" alt="ACM Logo" width={100} height={100} />
 
         <h1 className={styles.title}>
           <span className={styles.acm}>ACM</span> at UCSD
         </h1>
-      </div>
+      </a>
 
       <Socials />
     </div>
